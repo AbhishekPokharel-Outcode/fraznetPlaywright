2b28f9c44828e15f4acd609ea9c16cba3c8afced
Abhishek <abhishek.pokharel@outcodesoftware.com>
2024-11-14 22:10:25 +0545

    logout changes

--- utilities/logoutFraznet.spec.ts
+++ utilities/logoutFraznet.spec.ts
@@ -6,16 +6,16 @@ test.use({
         width: 1920
     }
 });
-test('test for random dropdown value + logout', async ({ page }) => {
+test('logout', async ({ page }) => {
 
 
-    await page.waitForSelector('body > div.main-panel > div > div.row.content-header-section > div > div > div:nth-child(1) > div.col-md-6', { state: 'visible' });
-    await page.click('#select2-filter_by_dist_retailer-container');
-
-    const options = await page.$$eval('#select2-filter_by_dist_retailer-results', spans => spans.map(span => span.textContent.trim()));
-    const randomOption = options[Math.floor(Math.random() * options.length)];
-    await page.selectOption('#select_dashboard', { label: randomOption });
-    console.log(`Selected option: ${randomOption}`);
+    // await page.waitForSelector('body > div.main-panel > div > div.row.content-header-section > div > div > div:nth-child(1) > div.col-md-6', { state: 'visible' });
+    // await page.click('#select2-filter_by_dist_retailer-container');
+    //
+    // const options = await page.$$eval('#select2-filter_by_dist_retailer-results', spans => spans.map(span => span.textContent.trim()));
+    // const randomOption = options[Math.floor(Math.random() * options.length)];
+    // await page.selectOption('#select_dashboard', { label: randomOption });
+    // console.log(`Selected option: ${randomOption}`);
     // await page.waitForTimeout(9000);
 
     await page.getByRole('link', { name: 'QA TESTER' }).click();
